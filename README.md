384c96cdfd7815ee2ae11bd8c3e550c212c255fe
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-04-04 21:42:50 +0300

    Simplify UI

--- README.md
+++ README.md
@@ -2,19 +2,21 @@ Library and a command-line interface for Tribler REST API
 
 (early version)
 
+At the moment it targets Tribler `v7.3.0-beta1`.
+
 Currently only some commands are implemented:
 
 ```
-$ tribler downloads list
+$ tribler list-downloads
  351.36 MiB    100%  2018-11-13-raspbian-stretch-lite.zip
 
-$ tribler search begin test
+$ tribler search test
    815.00 B test.log   magnet:?xt=urn:btih:98861d0ca6f44069a2c0e001e03bc95726b129de
    8.23 KiB test entry 102308   magnet:?xt=urn:btih:3430303738363637363637393233383537343830
      5.00 B test.txt   magnet:?xt=urn:btih:7de795fe731a31b2242e750d34974381ac2d53d8
 ...
 
-$ tribler search completions hel
+$ tribler search-complete hel
 helloween
 hellfire
 hellblazer
--- src/main.rs
+++ src/main.rs
@@ -9,82 +9,68 @@ use byte_unit::Byte;
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 enum Opt {
-    /// Operate on downloads
-    Downloads(Downloads),
-    /// Deal with searches
-    Search(Search)
-}
-
-#[derive(Debug, StructOpt)]
-#[structopt(rename_all = "kebab-case")]
-enum Downloads {
-    /// List downloads
-    List{},
-}
-
-#[derive(Debug, StructOpt)]
-#[structopt(rename_all = "kebab-case")]
-enum Search {
-    /// Search completions
-    Completions{
-        /// Prefix to complete
-        prefix: String,
-    },
-    /// Start new search
-    Begin {
+    /// Show short information about each download
+    ListDownloads,
+    /// Start searching for some query and give initial results
+    Search {
         /// Text to search in the torrent database for
         query: String,
 
         #[structopt(long="uuid",default_value="e09774f87c32414cad817e6d643a7235")]
         uuid: String,
-    }
+    },
+    /// Give list of search completions, one per line
+    SearchComplete {
+        /// Prefix to complete
+        prefix: String,
+    },
 }
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let opt = Opt::from_args();
 
-    let trib = tribler::TriblerRestApi::new("http://localhost:8085".to_string());
+    let tribler_default_urlbase = "http://localhost:8085";
+    let tribler_urlbase = std::env::var("TRIBLER_REST")
+            .unwrap_or(tribler_default_urlbase.to_string());
+
+    let trib = tribler::TriblerRestApi::new(tribler_urlbase);
     
     match opt {
-        Opt::Downloads(cmd) => match cmd {
-            Downloads::List{} => {
-                let ds = trib.get_downloads()?;
-                for d in ds {
-                    let s = Byte::from_bytes(d.size.into());
-                    println!(
-                        "{:>11} {:>6}%  {}",
-                        s.get_appropriate_unit(true).to_string(),
-                        d.progress * 100.0,
-                        d.name,
-                    );
-                }
-            },
-        },
-        Opt::Search(cmd) => match cmd {
-            Search::Completions { prefix } => {
-                let cs = trib.get_search_completions(&prefix)?;
-                for i in cs {
-                    println!("{}", i);
-                }
-            },
-            Search::Begin { query, uuid } => {
-                let q = tribler::SearchQuery::new(
-                    uuid,
-                    query,
+        Opt::ListDownloads => {
+            let ds = trib.get_downloads()?;
+            for d in ds {
+                let s = Byte::from_bytes(d.size.into());
+                println!(
+                    "{:>11} {:>6}%  {}",
+                    s.get_appropriate_unit(true).to_string(),
+                    d.progress * 100.0,
+                    d.name,
                 );
-                let srs = trib.begin_search(q)?;
-                for sr in srs {
-                    let sz = Byte::from_bytes(sr.size.into());
-                    println!(
-                        "{:>11} {}   magnet:?xt=urn:btih:{}", 
-                        sz.get_appropriate_unit(true).to_string(),
-                        sr.name,
-                        sr.infohash,
-                    );
-                }
             }
-        }
+        },
+        Opt::SearchComplete {prefix} => {
+            let cs = trib.get_search_completions(&prefix)?;
+            for i in cs {
+                println!("{}", i);
+            }
+        },
+        Opt::Search { query, uuid } => {
+            let q = tribler::SearchQuery::new(
+                uuid,
+                query,
+            );
+            let srs = trib.begin_search(q)?;
+            for sr in srs {
+                let sz = Byte::from_bytes(sr.size.into());
+                println!(
+                    "{:>11} {}   magnet:?xt=urn:btih:{}", 
+                    sz.get_appropriate_unit(true).to_string(),
+                    sr.name,
+                    sr.infohash,
+                );
+            }
+        },
     }
 
     Ok(())
